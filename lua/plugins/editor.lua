fac82f8f1b9a861b67126d52878fd48d3d9b7bf6
aliensarefake <guoxiaoyu2003429@gmail.com>
2025-07-06 20:29:15 +0800

    add auto-pairing of brackets etc

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -218,4 +218,39 @@ return {
       })
     end,
   },
+
+  -- Auto pairs
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = true,
+        ts_config = {
+          lua = { "string" },
+          javascript = { "string", "template_string" },
+          java = false,
+        },
+        disable_filetype = { "TelescopePrompt", "spectre_panel" },
+        fast_wrap = {
+          map = "<M-e>",
+          chars = { "{", "[", "(", '"', "'" },
+          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+          offset = 0,
+          end_key = "$",
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          check_comma = true,
+          highlight = "PmenuSel",
+          highlight_grey = "LineNr",
+        },
+      })
+      
+      -- If you have nvim-cmp installed, integrate with it
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      if cmp_status_ok then
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end
+    end,
+  },
 }
