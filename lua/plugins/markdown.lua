5f280517e42b7ba1b860716c388eb575f87a511e
aliensarefake <guoxiaoyu2003429@gmail.com>
2025-08-26 21:36:51 +0800

    support image pasting in nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -127,4 +127,59 @@ return {
       })
     end,
   },
+
+  -- Image rendering in Neovim
+  {
+    "3rd/image.nvim",
+    ft = { "markdown" },
+    config = function()
+      local ok, image = pcall(require, "image")
+      if not ok then
+        return
+      end
+      image.setup({
+        backend = "kitty",
+        integrations = {
+          markdown = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = false,
+            only_render_image_at_cursor = false,
+          },
+        },
+        max_width = nil,
+        max_height = nil,
+        max_width_window_percentage = nil,
+        max_height_window_percentage = 50,
+        window_overlap_clear_enabled = false,
+        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      })
+    end,
+  },
+
+  -- Clipboard image pasting
+  {
+    "HakonHarnes/img-clip.nvim",
+    ft = { "markdown" },
+    keys = {
+      { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from clipboard" },
+    },
+    config = function()
+      require("img-clip").setup({
+        default = {
+          dir_path = "./images",
+          file_name = "%Y-%m-%d-%H-%M-%S",
+        },
+        filetypes = {
+          markdown = {
+            relative_to_current_file = true,
+            dir_path = function()
+              return "images"
+            end,
+            template = "![$FILE_NAME_NO_EXT]($FILE_PATH)",
+          },
+        },
+      })
+    end,
+  },
 }
